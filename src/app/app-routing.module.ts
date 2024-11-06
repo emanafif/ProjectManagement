ad4854db1f9123e82be24a6d9a8e4f20ca6a869f
eman.it <eman.it@EMANIT>
2024-11-06 10:17:27 +0200

    prohectcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './Lists/projects/projects.component'; 
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AddnewprojectComponent } from './addnewproject/addnewproject.component'; 
 import { AddNewprojectComponent } from './add-newproject/add-newproject.component'; 
 
 const routes: Routes = [
 
   {path:"ProjectsComponent", component:ProjectsComponent},
-  {path:"", component:NavbarComponent},
   {path:"AddnewprojectComponent", component:AddnewprojectComponent},
   {path:"AddNewprojectComponent/:id", component:AddNewprojectComponent},
 
